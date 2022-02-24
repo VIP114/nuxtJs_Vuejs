840105d205c447c35b98ebbae8a65fdbeac97863
patelvishv114 <patelvishv343@gmail.com>
2022-02-24 15:06:12 +0530

    pagination type

---
+++ pages/videos/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-for="video in videos" :key="video.id">
+            {{video.name}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                videos:[{
+                    id:16,
+                    name: "a lkjdf dlj lad"
+                },
+                {
+                    id:1,
+                    name: "aadfadfaf lkjdf dlj lad"
+                },
+                {
+                    id:71,
+                    name: "adfadfafdfagfgfg lkdjsf j lkjdf dlj lad"
+                },
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
